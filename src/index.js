d6cabb123e995013a8591b373341772bd9bef5d2
sahilbisht <bishtrock890@gmail.com>
2024-11-22 11:07:36 +0530

    update in router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/global.css'
-
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+
+    </HashRouter>
   </React.StrictMode>
 );
 
